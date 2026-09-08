e962c46e783290e5b7d903e32af668edc80c3abd
annetbg-wq <annetdenr@gmail.com>
2026-09-09 00:57:53 +0400

    feat(web): mount live runtime metrics in observability

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { RuntimeMetricsMount } from "./runtime-metrics";
 import "./styles.css";
 import "./interaction-fixes.css";
+import "./runtime-metrics.css";
 
-createRoot(document.getElementById("root")!).render(<React.StrictMode><App/></React.StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App/>
+    <RuntimeMetricsMount/>
+  </React.StrictMode>,
+);
