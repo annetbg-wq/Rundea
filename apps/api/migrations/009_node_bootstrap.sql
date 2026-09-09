2001e7289b411550d24f11f204184d9745ef00d6
annetbg-wq <annetdenr@gmail.com>
2026-09-09 13:20:23 +0400

    refactor(api): use credential rotation for bootstrap

--- apps/api/migrations/009_node_bootstrap.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE nodes ADD COLUMN IF NOT EXISTS bootstrap_token_hash char(64);
-ALTER TABLE nodes ADD COLUMN IF NOT EXISTS bootstrap_expires_at timestamptz;
-ALTER TABLE nodes ADD COLUMN IF NOT EXISTS bootstrap_consumed_at timestamptz;
-
-CREATE INDEX IF NOT EXISTS nodes_bootstrap_expiry_idx
-  ON nodes(bootstrap_expires_at)
-  WHERE bootstrap_token_hash IS NOT NULL;
