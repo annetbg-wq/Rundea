3acc56ae3ac27ad6d7bfc7cf8475df9839229772
annetbg-wq <annetdenr@gmail.com>
2026-09-21 20:03:20 +0400

    feat(agent): advertise prebuilt image capability

--- apps/agent/identity.go
+++ apps/agent/identity.go
@@ -30,6 +30,7 @@ var declaredAgentCapabilities = []string{
 	"nodeDiskMetrics",
 	"nodeMaintenance",
 	"persistentVolumes",
+	"prebuiltImages",
 	"privateNetworking",
 	"resourceGuardrails",
 	"runtimeMetrics",
